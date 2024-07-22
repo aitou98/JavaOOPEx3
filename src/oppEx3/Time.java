9e172ebdf8361e216ee8da5cf022f030d1565ab5
yassine aitoukallal <yassine.aitoukallal@gmail.com>
2024-07-22 15:41:25 +0100

    first commit

---
+++ src/oppEx3/Time.java
@@ -0,0 +1,56 @@
+package oppEx3;
+
+public class Time {
+	
+	private int second;
+	private int minute;
+	private int hour;
+	
+	public Time() {
+		
+		
+	}
+	
+	public Time(int second , int  minute , int hour) {
+		
+		this.second = second;
+		this.minute = minute;
+		this.hour = hour;
+	
+	}
+	
+    public int getSecond() {
+		
+		return this.second;
+	}
+	
+    public int getMinute() {
+		
+		return this.minute;
+	}
+	
+	public int getHour() {
+		
+		return this.hour;
+	}
+	
+	public void setSecond(int second) {
+		
+		this.second = second;
+		
+		
+	}
+	
+	public void setMinute(int minute) {
+		
+		this.minute = minute;
+	}
+	
+	public void setHour(int hour) {
+		
+		this.hour = hour ; 
+	}
+	
+	
+
+}
